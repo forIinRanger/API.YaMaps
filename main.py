8c11ae9674c02644f8bf04a171177b8f175d796f
forIinRanger <ZhuraMax2006@yandex.ru>
2023-01-26 19:30:45 +0500

    move_screen

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+from io import BytesIO
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+import requests
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_image(x_coord: float, y_coord: float, size_x: float = 0.1, size_y: float = 0.1) -> BytesIO or str:
+    coords = ",".join([str(x_coord), str(y_coord)])
+    spn = ",".join([str(size_x), str(size_y)])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    search_api_server = "http://static-maps.yandex.ru/1.x/"
+
+    apikey = "40d1649f-0493-4b70-98ba-98533de7710b",
+
+    search_params = {
+        "l": 'map',
+        "apikey": apikey,
+        "ll": coords,
+        "spn": spn
+    }
+
+    response = requests.get(search_api_server, params=search_params)
+
+    if not response or response.status_code != 200:
+        return "Ошибка"
+
+    return BytesIO(response.content)
+
+
+def move_screen(i, x_coord: float, y_coord: float, spn: tuple[float, float]) -> BytesIO:
+    if i.type == pygame.K_UP:
+        return get_image(x_coord, y_coord + spn[1], spn[0], spn[1])
+    elif i.type == pygame.K_DOWN:
+        return get_image(x_coord, y_coord - spn[1], spn[0], spn[1])
+    elif i.type == pygame.K_RIGHT:
+        return get_image(x_coord + spn[0], y_coord, spn[0], spn[1])
+    elif i.type == pygame.K_LEFT:
+        return get_image(x_coord - spn[0], y_coord, spn[0], spn[1])
+
+    else:
+        return get_image(x_coord, y_coord, spn[0], spn[1])
