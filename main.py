f1cd3d7f52ef2cbd5765d6d2973e7d2c0af4baef
danilsdk <danil2006sosed@gmail.com>
2023-01-26 19:55:22 +0500

    update

--- main.py
+++ main.py
@@ -14,17 +14,23 @@ class App:
         self.surface = pygame.display.set_mode((self.width, self.height))
 
         pygame.display.set_caption("Yandex maps")
-        self.image = self.get_image(50, 50)
+
+        self.x_coord, self.y_coord = 50, 50
+        self.get_image(self.x_coord, self.y_coord)
 
     def run(self):
         while True:
+            map = pygame.transform.scale(self.image, (self.width, self.height))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    self.move_screen(event, self.x_coord, self.y_coord, (0.1, 0.1))
+            self.surface.blit(map, (0, 0))
+            pygame.display.flip()
 
-    @staticmethod
-    def get_image(x_coord: float, y_coord: float, size_x: float = 0.1, size_y: float = 0.1) -> BytesIO or str:
+    def get_image(self, x_coord: float, y_coord: float, size_x: float = 0.1, size_y: float = 0.1) -> BytesIO or str:
         coords = ",".join([str(x_coord), str(y_coord)])
         spn = ",".join([str(size_x), str(size_y)])
 
@@ -44,7 +50,21 @@ class App:
         if not response or response.status_code != 200:
             return "Ошибка"
 
-        return BytesIO(response.content)
+        self.image = pygame.image.load(BytesIO(response.content))
+
+    def move_screen(self, i, x_coord: float, y_coord: float, spn: (float, float)) -> BytesIO:
+        print(i.key)
+        if i.key == pygame.K_UP:
+            self.get_image(x_coord, y_coord + spn[1], spn[0], spn[1])
+        elif i.key == pygame.K_DOWN:
+            self.get_image(x_coord, y_coord - spn[1], spn[0], spn[1])
+        elif i.key == pygame.K_RIGHT:
+            self.get_image(x_coord + spn[0], y_coord, spn[0], spn[1])
+        elif i.key == pygame.K_LEFT:
+            self.get_image(x_coord - spn[0], y_coord, spn[0], spn[1])
+
+        else:
+            self.get_image(x_coord, y_coord, spn[0], spn[1])
 
 
 if __name__ == '__main__':
